b3753ef161381ef2362d045e5c870e4f15de31f8
leke <malekelthomas97@gmail.com>
2021-06-08 20:29:34 -0400

    logo links to sneakers pg

--- components/menu/logo.tsx
+++ components/menu/logo.tsx
@@ -1,13 +1,16 @@
 import React from "react"
 import { Box, Text} from "@chakra-ui/react"
+import Link from "next/link"
 
 const Logo = (props) => {
     return (
-        <Box {...props}>
-            <Text fontSize="lg" fontWeight="bold">
-                SNKRZ
-            </Text>
-        </Box>
+        <Link href="/sneakers">
+            <Box as="button" {...props}>
+                <Text fontSize="lg" fontWeight="bold">
+                    SNKRZ
+                </Text>
+            </Box>
+        </Link>
     )
 }
 
